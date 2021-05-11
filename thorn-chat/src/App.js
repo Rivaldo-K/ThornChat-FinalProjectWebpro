4742b0278fd7d8f8e10e46f98f4f6897b40076cf
Rivaldo-K <s21810158@student.unklab.ac.id>
2021-05-11 09:20:49 +0800

    sign in with google button and function

--- thorn-chat/src/App.js
+++ thorn-chat/src/App.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+// Components
+import Button from './components/Button';
 // Firebasedeps
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -12,9 +15,26 @@ firebase.initializeApp({
   appId: "1:918193578250:web:66d3596e8a31b4f78a5af3"
 });
 
+const auth = firebase.auth();
+
 function App() {
+const [user, setUser] = useState(() => auth.currentUser);
+
+  const signInWithGoogle = async () => {
+    // retrive google provider object
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // set language to default browser preference
+    auth.useDeviceLanguage();
+    //start sign in process
+    try {
+      await auth.signInWithPopup(provider);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div>
+      <button onClick={signInWithGoogle}>Sign in with google</button>
     </div>
   );
 }
