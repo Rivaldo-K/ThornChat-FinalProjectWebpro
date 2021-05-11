23b24d5b9161e390376155264bc42bba0950c520
Rivaldo-K <s21810158@student.unklab.ac.id>
2021-05-11 08:46:07 +0800

    setting up Authentication and create project firebase done

--- thorn-chat/src/App.js
+++ thorn-chat/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// Firebasedeps
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBtKnbRa2b021dcDv_pTjHUOy-YqujtZTc",
+  authDomain: "thorn-chat-ea7ef.firebaseapp.com",
+  projectId: "thorn-chat-ea7ef",
+  storageBucket: "thorn-chat-ea7ef.appspot.com",
+  messagingSenderId: "918193578250",
+  appId: "1:918193578250:web:66d3596e8a31b4f78a5af3"
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
